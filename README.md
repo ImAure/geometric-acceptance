637f7aa9eedcef5e2aca93309b73af7f3046da3f
Aurelio <pappalardoaurelio@gmail.com>
2025-05-23 01:52:32 +0200

    extended source completed

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
 # geometric-acceptance
-Simulation of the geometric acceptance of a cylindrical detector simply placed along z axis in the presence of an isotropic point source.
 
-## usage: 
-`./sim <number of points> <radius> <distance>`
+Monte Carlo evaluation of the geometric acceptance of a flat disc detector placed above an extended, emissive flat disc. Each point on the emissive disc is modeled as an independent isotropic source.  
 
-The program will return both the observed and the expected geometric acceptance.
+The user can control both the height of the detector and its horizontal offset from the vertical axis.
+
+### User Guide
+
+To compile the code, type the following in your Linux terminal:
+```
+gcc -o <path/to/executable> ext-source.c -lm
+```
+
+To run the simulation, use:
+```
+<path/to/executable> <number_of_points> <height> <horizontal_offset> <source_radius> <detector_radius>
+```
+All numbers must be positive, except for the offset, which can be negative.
+
+The program generates an output file at `tmp/out.txt` containing the full set of generated random points.  
+Each row in the file includes:
+
+- the x and y coordinates of the source point;  
+- the colatitude and longitude of the random radiation direction;  
+- a `1` or `0` indicating whether the ray intersects the detector.  
+
+### Other Files
+
+The remaining `.c` files are drafts for simplified scenarios that do not involve a horizontal offset or an extended source.  
+They are kept for reference and are not part of the main simulation.
---
+++ ext-source.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define ARG_NUM     6
+#define DATA_PATH   "./tmp/out.txt"
+#define E0    -1
+#define E1    1
+#define E2    2
+#define E3    3
+#define EF    4
+#define ER    5
+
+typedef struct direction {
+    double theta;
+    double phi;
+} direction;
+
+typedef struct polar {
+    double rho;
+    double phi;
+} polar;
+
+int rand_dir(direction *dest) {
+    if (dest == NULL) return -1;
+    dest->theta = acos(1 - 2 * ((double)rand() / RAND_MAX));
+    dest->phi = ((double)rand() / RAND_MAX) * (2 * M_PI);
+    return 0;
+}
+
+int rand_polar(polar *src, double max_rho) {
+    if (src == NULL) {
+        fprintf(stderr, "Error: argument is NULL.\n");
+        return -1;
+    }
+    if (max_rho <= 0) {
+        fprintf(stderr, "Error: max_rho must be greater than 0.\n");
+        return -1;
+    }
+    src->rho = sqrt(((double)rand() / RAND_MAX) * (max_rho * max_rho));
+    src->phi = ((double)rand() / RAND_MAX) * (2 * M_PI);
+    return 0;
+}
+
+int intcept(direction ray, double a, double b2, double r) {
+    double a2, b, r2, tg, tg2, sf;
+
+    if (ray.theta >= M_PI / 2) return 0;
+
+    a2  = a * a;
+    r2  = r * r;
+    b   = sqrt(b2);
+    tg  = sin(ray.theta) / cos(ray.theta);
+    sf  = sin(ray.phi);
+    tg2 = tg * tg;
+
+    return ((a2 * tg2 - (2 * a * b* tg * sf) + b2 - r2) <= 0);
+}
+
+int myexit(int status, FILE *pf, char *buffer) {
+    switch (status) {
+        case E0:
+            fprintf(stderr, "Usage: %s <n> <h> <d> <r> <R>\nWhere\n\tn: number of points to be generated (positive integer);\n\th: height of the detector (positive);\n\td: horizontal offset of the detector;\n\tr: radius of the source (non negative)\n\tR: radius of the detector (positive).\n", buffer);
+            break;
+        case E1:
+            fprintf(stderr, "Error: number of points must be a positive integer.\n");
+            break;
+        case E2:
+            fprintf(stderr, "Error: detector radius and height must be positive numbers.\n");
+            break;
+        case E3:
+            fprintf(stderr, "Error: source radius must be a non-negative number.\n");
+            break;
+        case EF:
+            fprintf(stderr, "Error: could not open output file.\n");
+            break;
+        case ER:
+            fprintf(stderr, "Error: random number generation failed.\n");
+            break;
+    }
+
+    if (pf != NULL) fclose(pf);
+    return status;
+}
+
+int main(int argc, char *argv[ARG_NUM]) {
+    double h, d, b2, r_src, r_det;
+    int n, i, hits;
+    FILE *pf;
+    direction ray;
+    polar source;
+
+    srand(time(NULL));
+    pf = NULL;
+
+    if (argc != ARG_NUM) return myexit(E0, pf, argv[0]);
+
+    pf    = fopen(DATA_PATH, "w");
+    n     = atoi(argv[1]);
+    h     = atof(argv[2]);
+    d     = atof(argv[3]);
+    r_src = atof(argv[4]);
+    r_det = atof(argv[5]);
+
+    if (n <= 0)               return myexit(E1, pf, argv[0]);
+    if (r_det <= 0 || h <= 0) return myexit(E2, pf, argv[0]);
+    if (r_src < 0)            return myexit(E3, pf, argv[0]);
+    if (pf == NULL)           return myexit(EF, pf, argv[0]);
+
+    for (i = 0, hits = 0; i < n; i++) {
+        if (rand_polar(&source, r_src)) return myexit(ER, pf, argv[0]);
+        if (rand_dir(&ray))             return myexit(ER, pf, argv[0]);
+
+        b2 = d * d + source.rho * source.rho - 2 * d * source.rho * sin(source.phi);
+
+        if (intcept(ray, h, b2, r_det)) {
+            hits++;
+            fprintf(pf, "%f %f %f %f 1", source.rho, source.phi, ray.theta, ray.phi);
+        } else {
+            fprintf(pf, "%f %f %f %f 0", source.rho, source.phi, ray.theta, ray.phi);
+        }
+        if (i != n - 1) fprintf(pf, "\n");        
+    }
+
+    fclose(pf);
+    fprintf(stdout, "Ratio: %.6f\n", (double)hits / n);
+    return 0;
+}
