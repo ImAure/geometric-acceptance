e83aeac14dfec369cfafab3af5b63364ab715639
Aurelio <pappalardoaurelio@gmail.com>
2025-05-22 22:45:32 +0200

    horizontal offset working

--- acc-hoffset.c
+++ acc-hoffset.c
@@ -3,7 +3,8 @@
 #include <math.h>
 #include <time.h>
 
-#define ARG_NUM 5
+#define ARG_NUM     5
+#define LOG_PATH    "./tmp/acc-hoffset.log"
 
 typedef struct direction {
     double theta;
@@ -17,24 +18,18 @@ int rand_dir(direction *dest) {
 }
 
 int intcept(direction ray, double a, double b, double r) {
-    double a2, b2, r2, d2, ba, ba2, st, ct, sf, st2, ct2, sf2;
-    if (a == 0) {
-        return 0;
-    }
+    double a2, b2, r2, tg, tg2, sf;
+
+    if (ray.theta >= M_PI / 2) return 0;
+
     a2  = a * a;
     b2  = b * b;
     r2  = r * r;
-    d2  = a2 + b2;
-    ba  = b / a;
-    ba2 = ba * ba;
-    st  = sin(ray.theta);
-    ct  = cos(ray.theta);
+    tg  = sin(ray.theta) / cos(ray.theta);
     sf  = sin(ray.phi);
-    st2 = st * st;
-    ct2 = ct * ct;
-    sf2 = sf * sf;
+    tg2 = tg * tg;
 
-    return ((d2 * st2 - r2 * ct2 + ba2 * sf2 * (d2 - r2) + 2 * r2 * ba * sf * st * ct) <= 0); 
+    return ((a2 * tg2 - (2 * a * b* tg * sf) + b2 - r2) <= 0);
 }
 
 int main(int argc, char *argv[ARG_NUM]) {
@@ -42,6 +37,7 @@ int main(int argc, char *argv[ARG_NUM]) {
     double a, b, r;
     int n;
     unsigned int hits, i;
+    FILE *log_file;
 
     srand(time(NULL));
 
@@ -49,7 +45,8 @@ int main(int argc, char *argv[ARG_NUM]) {
         fprintf(stderr, "Usage: %s <N> <a> <b> <R>\nWhere\n\tN: number of points to be generated (positive integer);\n\ta: height of the detector (positive);\n\tb: horizontal offset of the detector;\n\tR: radius of the detector (positive).\n", argv[0]);
         return 1;
     }
-    
+
+    log_file = fopen(LOG_PATH, "w");
     n = atoi(argv[1]);
     a = atof(argv[2]);
     b = atof(argv[3]);
@@ -60,15 +57,26 @@ int main(int argc, char *argv[ARG_NUM]) {
         return 1;
     }
     if (r <= 0 || a <= 0) {
-        fprintf(stderr, "Radius and distance must be positive numbers.\n");
+        fprintf(stderr, "Radius and height must be positive numbers.\n");
         return 1;
     }
-    for (i = 0, hits = 0; i < n; i++) {
-        rand_dir(&ray);
-        if (intcept(ray, a, b, r)) hits++;
+    if (log_file == NULL) {
+        fprintf(stderr, "Error opening log file.\n");
+        return 1;
     }
 
-    printf("Ratio: %.6f\n", (double)hits / (2 * n));
+    for (i = 0, hits = 0; i < n; i++) {
+        rand_dir(&ray);
+        if (intcept(ray, a, b, r)) {
+            hits++;
+            fprintf(log_file, "%f %f 1", ray.theta, ray.phi);
+        } else {
+            fprintf(log_file, "%f %f 0", ray.theta, ray.phi);
+        }
+        if (i != n - 1) fprintf(log_file, "\n");
+    }
+    
+    fclose(log_file);
+    printf("Ratio: %.6f\n", (double)hits / n);
     return 0;
 }
-
