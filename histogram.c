ffa6bbbeeb9d92f562161a369a355ecead5bf6aa
ImAure <pappalardoaurelio@gmail.com>
2025-05-27 01:41:05 +0200

    syncing with laptop

---
+++ histogram.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define ARG_COUNT   2
+
+int main(int argc, char *argv[]) {
+    
+    if (argc !=  ARG_COUNT) return -1;
+    
+    return 0;
+}
