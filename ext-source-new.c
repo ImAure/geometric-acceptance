b197c0953d06e8b0903d78e80a8c79100b963ec8
Aurelio <pappalardoaurelio@gmail.com>
2025-06-06 00:34:44 +0200

    update in new program, backup

--- ext-source-new.c
+++ ext-source-new.c
@@ -3,18 +3,15 @@
 #include <math.h>
 #include <time.h>
 
-#define DATA_PATH "./tmp/out.txt"
-#define ARG_NUM   5
+#define ARG_NUM 6
+#define STRLEN  1024
 
-#define HIT         " 1"
-#define MISS        " 0"
+#define HIT  " 1"
+#define MISS " 0"
 
-#define ERR_ARGC    -1
-#define ERR_FILE    -2
-#define ERR_NEG_NUM  1
-#define ERR_SRC_RAD  2
-#define ERR_DTC      3
-#define ERR_RAND     4
+#define ERR_ARGC -1
+#define ERR_FILE -2
+#define ERR       1
 
 typedef struct _dir3D {
     double theta;
@@ -25,6 +22,14 @@ typedef struct _polar2D {
     double rho;
     double phi;
 } polar2D_t;
+
+typedef struct _point3D {
+    double x;
+    double y;
+    double z;
+} point3D_t;
+
+typedef FILE file_t;
 
 int rand_dir3D_gen(dir3D_t *ptr) {
     if (ptr == NULL) return -1;
@@ -37,7 +42,7 @@ int rand_polar2D_gen(polar2D_t *ptr, double max_rho){
     if ((ptr == NULL) || (max_rho < 0)) return -1;
     if (max_rho < 0) {
         ptr->rho = 0;
-        prt->phi = 0;
+        ptr->phi = 0;
     } else {
         ptr->rho = sqrt(((double)rand() / RAND_MAX) * (max_rho * max_rho));
         ptr->phi = ((double)rand() / RAND_MAX) * (2 * M_PI);
@@ -45,85 +50,93 @@ int rand_polar2D_gen(polar2D_t *ptr, double max_rho){
     return 0;
 }
 
-int intcept(void) {}
+int intcept(dir3D_t ray, double height, double offset, double radius) {
+    double r2, u, u2, sin_theta, cos_theta;
+    
+    if (height == 0) return 0;
 
-int myexit(int status, FILE *pf, char *string) {
-    (void)fprintf(stderr, "An error occurred in executing '%s':\n", string);
+    r2 = radius * radius;
+    u = (offset / height) * sin(ray.phi);
+    u2 = u * u;
+    sin_theta = sin(ray.theta);
+    cos_theta = cos(ray.theta);
+
+    return ((sin_theta * sin_theta * ((height * height + offset * offset) * (1 + u2) + r2 * (1 - u2)) - r2 + 2 * u * r2 * sin_theta * cos_theta) <= 0);
+}
+
+int cartesian_print(file_t pf, dir3D_t ray, polar2D_t src_polar, double height) {
+    point3D_t src, dtc;
+
+
+}
+
+int myexit(const int status, file_t *pf1, file_t *pf2, const char *message) {
+    (void)fprintf(stderr, "An error occurred:\n");
     switch (status) {
         case ERR_ARGC:
-            (void)fprintf(stderr, "Usage: %s <n> <r> <h> <d> <R>\nWhere\n", string);
+            (void)fprintf(stderr, "Usage: %s <n> <r> <h> <d> <R> <output_file>\nWhere\n", message);
             (void)fprintf(stderr, "\tn: number of points to be generated (positive integer);\n");
             (void)fprintf(stderr, "\tr: radius of the source (cm, non negative)\n");
             (void)fprintf(stderr, "\th: height of the detector (cm, positive);\n");
             (void)fprintf(stderr, "\td: horizontal offset of the detector (cm);\n");
             (void)fprintf(stderr, "\tR: radius of the detector (positive).\n");
             break;
-        case ERR_NEG_NUM:
-            (void)fprintf(stderr, "Number of points must be a positive integer.\n");
-            break;
-        case ERR_SRC_RAD:
-            (void)fprintf(stderr, "Source radius must be a non-negative number.\n");
-            break;
-        case ERR_DTC:
-            (void)fprintf(stderr, "Detector radius and height must be positive numbers.\n");
-            break;
         case ERR_FILE:
-            (void)fprintf(stderr, "Could not open output file.\n");
-            break;
-        case ERR_RAND:
-            (void)fprintf(stderr, "Random number generation failed\n");
+            (void)fprintf(stderr, "Could not open output file at '%s'\n", message);
             break;
         default:
-            (void)fprintf(stedd, "Unknown error\n");
+            (void)fprintf(stderr, "%s\n", message);
+            break;
     }
 
-    if (pf != NULL) fclose(pf);
+    if (pf1 != NULL) fclose(pf1);
+    if (pf2 != NULL) fclose(pf2);
     return status;
 }
 
 int main(int argc, char *argv[]) {
     double src_radius, dtc_height, dtc_offset, dtc_radius;
+    double equiv_offset;
     int n, i, hits;
-    char buffer[STRLEN];
-    FILE *pf;
+    char buffer[STRLEN], file1[STRLEN], file2[STRLEN];
+    file_t *pf1;
     dir3D_t ray;
     polar2D_t src_point;
 
     srand(time(NULL));
-    pf = NULL;
-    
-    if (argc =! (ARG_NUM + 1)) return myexit(ERR_ARGC, pf, argv[0]);
+    pf1 = NULL;
+    pf2 = NULL;
 
-    pf = fopen(DATA_PATH, "w");
-    n  = atoi(argv[1]);
+    if (argc != (ARG_NUM + 1)) return myexit(ERR_ARGC, pf1, pf2, argv[0]);
+
+    n = atoi(argv[1]);
     src_radius = atof(argv[2]);
     dtc_height = atof(argv[3]);
     dtc_offset = atof(argv[4]);
     dtc_radius = atof(argv[5]);
 
-    if (n <= 0)         return myexit(ERR_NEG_NUM, pf, argv[0]);
-    if (src_radius < 0) return myexit(ERR_SRC_RAD, pf, argv[0]);
+    sprintf(file1, "%s", argv[6]);
+    pf1 = fopen(file1, "w");
+    sprintf(file2, "hits-cart-%s", argv[6]);
+    pf2 = fopen(file2, "w");
+
+    if (n <= 0)         return myexit(ERR, pf1, pf2, "Number of points must be a positive integer.");
+    if (src_radius < 0) return myexit(ERR, pf1, pf2, "Source radius must be a non-negative number.");
     if ((dtc_radius <= 0) || (dtc_height <= 0))
-                        return myexit(ERR_DTC, pf, argv[0]);
-    if (pf == NULL)     return myexit(ERR_FILE, pf, argv[0]);
+                        return myexit(ERR, pf1, pf2, "Detector radius and height must be positive numbers.");
+    if (pf1 == NULL)    return myexit(ERR_FILE, pf1, pf2, file1);
+    if (pf2 == NULL)    return myexit(ERR_FILE, pf2, pf2, file2);
 
     for (i = 0, hits = 0; i < n; i++) {
-        if (rand_pola2D_gen(&src_point, src_radius)) return myexit(ERR_RAND, pf, argv[0]);
-        if (rand_dir3D_gen(&ray))                    return myexit(ERR_RAND, pf, argv[0]);
-        (void)sprintf(buffer, "%f %f %f %f", src_point.rho, src_point.phi, ray.theta, ray.phi)
-        /*
-            WRITE INTCEPT FUNCTION
-        */
-        if(intcept()) {
+        if (rand_polar2D_gen(&src_point, src_radius) || rand_dir3D_gen(&ray)) return myexit(ERR, pf1, pf2, "Random number generation failed.");
+
+        (void)fprintf(pf1, "%f %f %f %f", src_point.rho, src_point.phi, ray.theta, ray.phi);
+        if(intcept(ray, height, equiv_offset, dtc_radius)) {
+            if (hits == 0) fprintf()
+            (void)cartesian_print(pf2, ray, src_point, height);
             hits++;
-            (void)strcat(buffer, HIT);
-        } else {
-            (void)strcat(buffer, MISS);
         }
 
-        if (i != n - 1) strcat(buffer, "\n");
-
-        fprintf(pf, "%s", buffer);
+        if (i != n - 1) fprintf(pf1, "\n");
     }
-
 }
